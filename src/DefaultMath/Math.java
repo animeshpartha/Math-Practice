d58a992f92e30a07f6c2d5036e5aa53388ccc4bf
animeshpartha <partha_uk@hotmail.com>
2019-01-02 01:44:50 -0500

    Math Practice

---
+++ src/DefaultMath/Math.java
@@ -0,0 +1,22 @@
+package DefaultMath;
+
+public class Math {
+	
+	int first = 10;
+	int second = 20;
+	int third = 30;
+	int forth = 40;
+	int fifth = first+second;
+	int sixth = third+forth;
+	
+	
+	public void add() {
+		System.out.println(fifth);
+
+	}
+	
+	public void subt() {
+		System.out.println(sixth);
+
+}
+}
---
+++ src/DefaultMath/Testmath.java
@@ -0,0 +1,13 @@
+package DefaultMath;
+
+public class Testmath {
+
+	public static void main(String[] args) {
+		Math m1 = new Math();
+		m1.add();
+		m1.subt();
+		
+
+	}
+
+}
